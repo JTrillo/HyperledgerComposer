dbc8e34f7a1129cb9f72316e42f0ab8f0fb7c11b
JTrillo <jokinator20@fivenet-1.europe-west1-b.c.hyperledger-jte.internal>
2019-09-17 16:40:52 +0000

    Script for participants creation

---
+++ nuclear_auto/dist/createParticipants.sh
@@ -0,0 +1,6 @@
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "mdr", "passport": "11111111A", "name": "Manuel Díaz", "nationality": "Spanish", "role": "ADMIN"}' -c admin@nuclear_auto
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "alba", "passport": "22222222B", "name": "Iván Alba", "nationality": "Spanish", "role": "ACQUISITOR"}' -c admin@nuclear_auto
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "auto", "role": "AUTO"}' -c admin@nuclear_auto
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "esc", "passport": "33333333C", "name": "Enrique Soler", "nationality": "Spanish", "role": "ANALYST"}' -c admin@nuclear_auto
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "llopis", "passport": "44444444D", "name": "Luís Llopis", "nationality": "Spanish", "role": "ANALYST"}' -c admin@nuclear_auto
+composer participant add -d '{"$class": "ertis.uma.nuclear.Staff", "participantId": "trillo", "passport": "55555555E", "name": "Joaquín Trillo", "nationality": "Spanish", "role": "ADVANCED_ANALYST"}' -c admin@nuclear_auto
